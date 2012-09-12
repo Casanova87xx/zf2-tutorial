51d880acc802ddb284d5bc81471e8edb7df49050
Marcos Vidal Fernández Heras <marcosvidal1987@gmail.com>
2012-09-12 15:22:05 +0200

    Añadiendo indexAction

--- module/Album/src/Album/Controller/AlbumController.php
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -12,7 +12,9 @@ class AlbumController extends AbstractActionController {
     protected $albumTable;
 
     public function indexAction() {
-        
+        return new ViewModel(array(
+                    'albums' => $this->getAlbumTable()->fetchAll(),
+                ));
     }
 
     public function addAction() {
